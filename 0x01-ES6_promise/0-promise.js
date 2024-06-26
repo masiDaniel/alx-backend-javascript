3d309bb919592fcc7a7142fc4db91558bf509fdc
masiDaniel <masidaniel02@gmail.com>
2024-06-26 22:19:12 +0300

    first commit in this directory

---
+++ 0x01-ES6_promise/0-promise.js
@@ -0,0 +1,11 @@
+/*
+ * Defines a function getResponseFromAPI
+ * which returns a Promise
+ */
+
+export default function getResponsefromAPI() {
+  return new Promise((resolve, reject) => {
+    resolve();
+    reject();
+  });
+}
---
+++ 0x01-ES6_promise/1-promise.js
@@ -0,0 +1,11 @@
+// Defines a function getFullResponseFromAPI(success)
+
+export default function getFullResponseFromAPI(success) {
+  return new Promise((resolve, reject) => {
+    if (success === true) {
+      resolve({ status: 200, body: 'Success' });
+    } else {
+      reject(new Error('The fake API is not working currently'));
+    }
+  });
+}
